84fdcd072cd4b58d1a5c33846cbe9df560fe6cc0
billyshears314 <awfilipek@gmail.com>
2020-02-24 22:38:03 -0500

    change react to vue app

---
+++ src/components/Project.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Project: {{name}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Project',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
